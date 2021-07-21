965fbbe92438c326c00c4a41122d462e33a64c8e
Daniel Dipper <daniel.dipper@sage.com>
2021-07-21 14:30:50 +0100

    fix(flat-table): ensure correct z-index is applied to flat-table-header
    z-index has been increased to 1002 when hasStickyHead to prevent content being hidden on scrolling
    horizontally
    
    fixes #4226

--- src/components/flat-table/flat-table-header/flat-table-header.style.js
+++ src/components/flat-table/flat-table-header/flat-table-header.style.js
@@ -70,6 +70,9 @@ const StyledFlatTableHeader = styled.th`
       css`
       left: ${leftPosition}px;
       position: sticky;
+      &&& {
+        z-index: 1002;
+      }
 
       &:first-child {
         padding-right: 0.395em;
